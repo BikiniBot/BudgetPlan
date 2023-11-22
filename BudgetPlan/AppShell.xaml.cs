a8eebf38e3c05b42844458b3aee36dc30b734e82
Ikki Kariu <alexander1stcm@gmail.com>
2023-11-22 14:23:58 +0700

    Добавьте файлы проекта.

---
+++ BudgetPlan/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetPlan
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BudgetPlan/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BudgetPlan
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
